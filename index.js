edb964d6d17da0e1e756213d1f733acddc69958a
Gregory Pounds <greg.pounds3@gmail.com>
2023-02-02 17:45:22 -0600

    Fixed reset plus design! Add to qoutes that are random!

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const guessButton = document.querySelector("#guess-button")
 const hintButton = document.querySelector("#hint-button")
 const resetButton = document.querySelector("#reset-button")
 const guessList = document.querySelector("ul");
+const h4 = document.querySelector("h4");
 
 const guessesArr =[]
 const livesStart =3
 let guessCount = 0
 const correctAnswer = Math.floor(Math.random() * 101);
+
+//Code for random qoute
+const qouteArr= ["You don't know when to quit, do ya Griswold?","It's all part of the act, Russ.","Have you ever swam with the dolphins? Them things are hard to catch","Yeah, where can I get some damn bait?"]
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 console.log(correctAnswer)
 
     guessButton.addEventListener("click", function(){
+        let randomQoute = Math.floor(Math.random() * 4);
         const currentGuess = numField.value;
         console.log(currentGuess)
         if (guessCount < livesStart){
@@ -27,20 +33,23 @@ console.log(correctAnswer)
         passGuesses.textContent = currentGuess
         guessList.appendChild(passGuesses)
         guessesArr.push(currentGuess)
+        h4.textContent = (qouteArr[randomQoute])
         } else {
             alert("I haven't seen a beatin' like that since somebody stuck a banana in my pants and turned a monkey loose.");
         }
 
         console.log(`You have "${livesStart - guessCount}" More Tries!`)
-        console.log("Tries You Made-" + guessCount)
+        console.log("Tries You Made- " + guessCount)
         console.log(guessesArr) 
 });
 
 hintButton.addEventListener("click", function(){
     console.log("You Suck!!!!!!!!!!!!!!!")
+//Should help user get closer to the right number
 })
 resetButton.addEventListener("click", function(){
     guessCount = 0
-    browser.menus.refresh()
+    // browser.menus.refresh()
+    document.location.reload()
 })
 
